d3ff41c2c5a6b7f81c4b63d979a1c150e4a9acf4
Sean <tordynamics@gmail.com>
2020-01-21 23:53:17 +0800

    changed ip

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.82.55.34',
+      host: '34.205.19.93',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
